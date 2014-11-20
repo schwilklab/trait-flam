c1534ea123a92405f90d0e4222620ee42b97e612
Dylan W. Schwilk <dylan@schwilk.org>
2014-11-20 16:26:06 -0600

    Refactor decomp R scripts
    1. Create single script to read and summarize data, read-decomp.R
    2. Create source file for moment functions
    3. Rename other files with more descriptive names, remove spaces
    4. Modify scripts for code re-use, clean up
    
    NOTE: the decomp-plots.R scrip needs work. I did not take the time to
    figure out what that script is attempting.

---
+++ scripts/decomp-plots.R
@@ -0,0 +1,97 @@
+library(ggplot2)
+
+
+## DWS: I don't really get this script, but am including it for now. Ther are a
+## lot of porblems with tis code. Fr now, I'm just trying to refactor, however.
+
+
+source("decomp-flam.R")
+
+## Some ggplot theme elements
+bestfit <- geom_smooth(method="lm",se = F, color = "black", size=1.5)
+isoline <- geom_abline(color="black", size=1.5, linetype="dashed") 
+textsize <- 20
+themeopts <-  theme(axis.title.y = element_text(size = textsize, angle = 90, vjust=0.3),
+                    axis.title.x = element_text(size = textsize, vjust=-0.3),
+                    panel.background = element_rect(size = 2),
+                    axis.text.x  = element_text(size=20),
+                    axis.text.y  = element_text(size=20),
+                    panel.grid.minor = element_line(colour = NA),
+                    panel.grid.major = element_line(colour = NA)
+                    )
+
+
+## DWS: note that plots and models below have problem: spread rate does not
+## vary within species.  So pseudoreplicated.
+ggplot(flamy0, aes(l.mean, spread.mean)) +
+    geom_point(size=3) +
+    geom_errorbar(aes(ymin=spread.mean-spread.se, ymax=spread.mean+spread.se), size=0.5)+
+    scale_x_continuous(expression(paste("Leaf length (mm)"))) + #, limits=c(0.0, 2000)) +
+    scale_y_continuous("Spread rate (cm/s)") + #, limits=c(0.0,0.5)) +
+       #bestfit +  # NOte this is not the correct line. you need to pull the coef from the model.
+       #geom_smooth(method="lm", se=F, size=1.5, colour="black") + 
+    scale_linetype_manual(values = c(1,3,2)) +
+    scale_color_manual(values = c( "gray50", "black")) +
+    theme_bw() + themeopts + geom_text(aes(label=spcode),hjust=0, vjust=0)
+	   
+lmfit <- lm(spread.mean~l.mean, data=flamy0)
+summary(lmfit)
+
+lmfit2 <- lm(spread.mean~l.mean + I(l.mean^2), data=flamy0)
+summary(lmfit2)
+
+
+flamy0q <- subset(flamy0, spcode!="Quke")
+
+ggplot(flamy0q, aes(l.mean, spread.mean)) +
+       geom_point(size=3) 
+
+lmfit3 <- lm(spread.mean~l.mean, data=flamy0q)
+summary(lmfit3)
+
+lmfit4 <- lm(spread.mean~l.mean + I(l.mean^2), data=flamy0q)
+summary(lmfit4)
+
+#Residual standard error: 0.03828 on 4 degrees of freedom
+#Multiple R-squared: 0.8706,     Adjusted R-squared: 0.8059 
+#F-statistic: 13.45 on 2 and 4 DF,  p-value: 0.01675
+
+
+## DWS: why are there hard-coded coefficients, below. use predict()!
+
+beta <- coef(lmfit4) #extracts the coefficients for the model
+beta
+fitcurve <- function(x) {-0.6192 + 0.01405*x -0.00005043*x^2}
+
+new.data <- subset(decomp.sum, year=="1", select=c(spcode, l.mean))
+
+new.data$spread.pred <- (-0.6192 + 0.01405*(new.data$l.mean) -0.00005043*((new.data$l.mean)^2))
+new.data$spread.pred[new.data$spread.pred < 0] = 0
+
+mycolours <- c("#FF6600", "#CC0000", "#660066", "#000999", "#3366CC",
+               "#00CCFF", "#00FF33", "#006600")
+
+a <- ggplot(flamy0q, aes(l.mean)) +
+    geom_point(aes(y=spread.mean), colour="blue") +
+    geom_text(aes(y=spread.mean, label=spcode),hjust=0, vjust=0) +
+    geom_errorbar(aes(ymin=spread.mean-spread.se, ymax=spread.mean+spread.se),
+                  size=0.5) +
+    scale_x_continuous(expression(paste("Mean leaf length (mm)")),
+                      limits=c(0, 205)) +
+    scale_y_continuous("Mean spread rate (cm/s)", limits=c(0, 0.33)) +
+    themeopts + theme_bw()
+
+
+
+b <- ggplot(new.data, aes(l1.mean, spread.prd, colour=spcode)) + geom_boxplot(alpha=0, lwd=1) + scale_colour_manual(values=mycolours, name="Species", breaks=c("Abco", "Abma", "Cade", "Pije", "Pila", "Pipo", "Quke", "Segi"), labels=c(expression(italic("Abies concolor")), expression(italic("Abies magnifica")), expression(italic("Calocedrus decurrens")), expression(italic("Pinus jeffreyi")), expression(italic("Pinus lambertiana")), expression(italic("Pinus ponderosa")), expression(italic("Quercus kelloggii")), expression(italic("Sequoiadendron giganteum")))) + scale_x_continuous(expression(paste("Mean leaf length (mm)")), limits=c(0, 205)) + scale_y_continuous("Mean spread rate (cm/s)")+ themeopts + theme_bw() + theme(legend.justification=c(1,0), legend.position=c(1,0), legend.text = element_text(colour="black", size = 14, face = "italic"), legend.title = element_text(colour="black", size=14, face="bold"))
+
+## DWS: wrap!
+
+library(gridExtra)
+ppi <- 300
+png("Spread vs length2.png", width=10*ppi, height=15*ppi, res=ppi)
+grid.arrange(a, b, ncol=1)
+grid.text("Year 0",0.91,0.95, gp=gpar(fontsize=20))
+grid.text("Year 1",0.91,0.45, gp=gpar(fontsize=20))
+dev.off()
+
