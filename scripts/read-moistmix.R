5db8f0b1e60b86adf00037b04a15fd25634c5e6e
FieryMedusa <rita.quinones-magalhaes@ttu.edu>
2015-11-02 10:51:34 -0600

    Added file to read the burn data from mixtures
    Script was added to read in the csv file that has all the data for the
    burn trials performed on mixtures

---
+++ scripts/read-moistmix.R
@@ -0,0 +1,22 @@
+# read data
+mmc <- read.csv("../data/moisture/burn_moisture_mixtures.csv")
+
+source("theme-opts.R")
+
+xbreaks <- seq(0, 144, 12)
+ybreaks <- seq(0, 700, 50)
+
+png("moisturemix.png", width=9*ppi, height=5*ppi, res=ppi)
+
+ggplot(mmc, aes(drying_time, MC_dry, colour=spcode)) +
+  geom_point(size=1.5) +
+  scale_colour_discrete(name="Species", breaks=mspbreaks) +
+  xlab("Hours since dry-down") + ylab("Moisture by dry weight (%)") +
+  scale_x_continuous(breaks=xbreaks) +
+  scale_y_continuous(breaks=ybreaks) +
+  theme_bw()+ 
+  geom_smooth(method="glm", family=gaussian(link="log"))
+
+dev.off()
+
+ggsave("../results/plots/moisturemix.png", width=9, height=5, dpi=ppi)
