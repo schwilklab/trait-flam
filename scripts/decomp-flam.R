bd4b3052098c44511f154465d6c9d424425ddcda
Dylan W. Schwilk <dylan@schwilk.org>
2015-11-03 14:35:27 -0600

    Refactor decomposition data summaries

--- scripts/decomp-flam.R
+++ scripts/decomp-flam.R
@@ -15,12 +15,8 @@ library(AICcmodavg)
 
 # DWS: you should fit on original flam data in a nested model. By fitting on
 # means you are overestimating certainty. But may be fine for now
-flamdecomp.sum <- flamdecomp %>%
-    select(spcode, bulk.mean, l_mean, w_mean, t_mean, spread.mean, larea_mean) %>%
-    group_by(spcode) %>%
-    summarise_each(funs(mean))
 
-lmfit <- lm(spread.mean~ larea_mean*t_mean, data=flamdecomp.sum)
+lmfit <- lm(spread.mean ~ larea_mean*t_mean, data=flamdecomp.sum)
 summary(lmfit)
 
 # just "thinness"
@@ -31,25 +27,33 @@ summary(lmfit2)
 aictab(list(lmfit, lmfit2))
 
 # so simple model wins.
-# does this mean bulk density is fucntion oflength of thickness?
-ggplot(flamdecomp.sum, aes(l_mean/t_mean, bulk.mean)) + geom_point()
+# does this mean bulk density is function of length over thickness?
+ggplot(flamdecomp.sum, aes(l_mean/t_mean, bulk.mean)) + geom_point() +
+    geom_smooth(method="lm", se=FALSE)
+ggplot(flamdecomp.sum, aes(l_mean/t_mean, spread.mean)) + geom_point()
+ggplot(flamdecomp.sum, aes(bulk.mean, spread.mean)) + geom_point()
 # yes.
 
 # Creating new dataframe with the predicted spread rate after decomposition (year 1)
 
-pred.y1 <- decomp.sum %>%  filter(year==1) %>%
+pred.y1 <- decomp.sum %>%  filter(year==1) %>% ungroup() %>%
     select(spcode, l_mean, w_mean, t_mean, larea_mean)
 
-pred.y1$spread.mean <- predict(lmfit2, newdata=pred.y1)
+pred.y1$pred.spread <- predict(lmfit2, newdata=pred.y1)
+#pred.y1$ci.spread.y1 <- predict(lmfit2, newdata=pred.y1, interval="confidence")
 
-pred.y1 <- pred.y1 %>% select(spcode, spread.mean, l_mean, w_mean, larea_mean, t_mean) %>%
-    group_by(spcode) %>%
-    summarise_each(funs(mean))
+
+pred.y1 <- pred.y1 %>% select(spcode, pred.spread, l_mean, w_mean, larea_mean, t_mean) %>%
+    group_by(spcode) %>% summarise_each(funs(mean, sd))
 
 flamdecomp.sum$pred.spread <- predict(lmfit2) # pred fitted values
 
-ggplot(flamdecomp.sum, aes(l_mean/t_mean, spread.mean)) +
-    geom_point(size=3, color="black")  +
-    geom_point(data=pred.y1, size=3,color="gray50") +
-    geom_point(aes(l_mean/t_mean, pred.spread), size=3, color="blue")
+## flamdecomp.sum <- left_join(flamdecomp.sum, select(pred.y1, spcode, pred.spread_mean, pred.spread_sd), by="spcode")
+
+## ggplot(flamdecomp.sum, aes(l_mean/t_mean, spread.mean)) +
+##     geom_point(size=3, color="black")  +
+##     geom_point(data=pred.y1, size=3,color="gray50") +
+##     geom_point(aes(l_mean/t_mean, pred.spread), size=3, color="blue")
+
+# todo: add CI to predictions
 
--- scripts/read-decomp.R
+++ scripts/read-decomp.R
@@ -5,7 +5,6 @@
 ## 3. Exports three data frames, "decomp", "decomp.sum" and "decomp.sum2"
 
 source("read-flam.R")
-
 library(dplyr)
 
 # for kurtosis() function:
@@ -13,9 +12,9 @@ source("dist-funcs.R")
 
 
 ## The files are separate due to their size 
-Y0 <- read.csv("../data/decomp/Decomp_Y0.csv", na.strings = c("","NA"))
-Y1 <- read.csv("../data/decomp/Decomp_Y1.csv", na.strings = c("","NA"))
-Y2 <- read.csv("../data/decomp/Decomp_Y2.csv", na.strings = c("","NA"))
+Y0 <- read.csv("../data/decomp/Decomp_Y0.csv", na.strings = c("","NA"), stringsAsFactors=FALSE)
+Y1 <- read.csv("../data/decomp/Decomp_Y1.csv", na.strings = c("","NA"), stringsAsFactors=FALSE)
+Y2 <- read.csv("../data/decomp/Decomp_Y2.csv", na.strings = c("","NA"), stringsAsFactors=FALSE)
 
 # rbind em up
 decomp <- bind_rows(Y0,Y1,Y2)
@@ -24,7 +23,7 @@ rm(list = c("Y0","Y1", "Y2"))
 
 #decomp$year <- factor(decomp$year)
 #decomp$tag <- factor(decomp$tag)  # dangerous -- will result in merge errors
-
+decomp <- filter(decomp, ! (tag==126 & year > 0))
 decomp$larea <- decomp$l * decomp$w
 decomp$lvol <- decomp$larea * decomp$t
 
@@ -38,6 +37,11 @@ q90 <- function(x) { quantile(x, .90, na.rm=TRUE)}
 q10 <- function(x) { quantile(x, .10, na.rm=TRUE)} 
 N <- function(x) {sum(!is.na(x))}
 
+wt <- read.csv("../data/decomp/Decomp_weight.csv", na.strings = c("","NA"), stringsAsFactors=FALSE)
+wt <- filter(wt, tag!=126)
+wt$drate <- -log(wt$wf/wt$wi)/wt$year
+
+# summarize by litter bag
 decomp.sum <- decomp %>% group_by(tag, spcode, year, alt, asp) %>%
     summarize_each(funs(mean(., na.rm=TRUE),
                         sd(., na.rm=TRUE),
@@ -46,14 +50,28 @@ decomp.sum <- decomp %>% group_by(tag, spcode, year, alt, asp) %>%
                         IQR(., na.rm=TRUE), # IQR(l) Interquartile range (3Q-1Q)
                         kurtosis(., na.rm=TRUE)
                         )
-                   )
+                   ) %>%
+    left_join(wt) %>%
+    left_join(species)
 
+# just year zero with flam trial results by species
+## flamdecomp <- decomp.sum %>% filter(year==0) %>% select(l_mean, t_mean, display.name) %>%
+##     group_by(display.name) %>% summarize(lt_mean = mean(l_mean/t_mean, na.rm=TRUE),
+##                                          lt_sd=sd(l_mean/t_mean, na.rm=TRUE))
+flamdecomp <- merge(flam.sp.avg, decomp.sum, all.x=TRUE)
 
-wt <- read.csv("../data/decomp/Decomp_weight.csv", na.strings = c("","NA"))
-wt$drate <- -log(wt$wf/wt$wi)/wt$year
-
-decompwt <- left_join(decomp.sum, wt)
-
-flamdecomp <- merge(flam.sp.avg, subset(decomp.sum, year == "0"),
-                    by="spcode", sort=F, all.x=TRUE)
+flamdecomp.sum <- flamdecomp %>% filter(year==0) %>%
+    select(display.name, bulk.mean, bulk.se, spread.mean, spread.se,
+           l_mean, w_mean, t_mean, spread.mean, larea_mean) %>%
+    group_by(display.name) %>% summarise(bulk.mean = mean(bulk.mean),
+                                         bulk.se = mean(bulk.se),
+                                         spread.mean = mean(spread.mean),
+                                         spread.se = mean(spread.se),
+                                         larea_mean = mean(larea_mean),
+                                         t_mean = mean(t_mean),
+                                         l_mean = mean(l_mean),
+                                         w_mean = mean(w_mean),
+                                         lt_mean= mean(l_mean/t_mean),
+                                         lt_sd  = sd(l_mean/t_mean)
+                                         )
 
