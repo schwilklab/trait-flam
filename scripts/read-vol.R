8bf4d97e70bebdeb927f1603d628faaa42295616
Dylan W. Schwilk <dylan@schwilk.org>
2015-11-04 14:50:58 -0600

    Move read-vol to tidyr and dplyr

--- scripts/read-vol.R
+++ scripts/read-vol.R
@@ -2,26 +2,27 @@
 
 ## read-vol.R
 
+## Volatiles data.
+
 ## 1. Read in files, turn from wide to long
 ## 2. Create summary statistics data frame
 
-library(reshape2)
-library(plyr)
+library(tidyr)
+library(dplyr)
 
 vol_wide <- read.csv("../data/volatiles/vol_wide.csv", na.strings = c("","NA"))
 
-vol <- melt(vol_wide, id.vars=c("name","group","flash_point"), 
-            variable.name = "spcode", 
-            value.name = "area")
+vol <- vol_wide %>% gather(key=spcode, value=area, -name, -group, -flash_point) %>%
+    separate(spcode, c("spcode", "replicate"), "\\.") %>%
+    select(spcode, replicate, name, group, flash_point, area) %>%
+    filter(complete.cases(.))
 
-#sapply(vol, class)
+vol.sum <- vol %>% group_by(spcode, replicate, group) %>%
+    summarise(sum_area=sum(area), fp_wmean=weighted.mean(flash_point, area),
+              sum_low_fp = sum(area[flash_point < 100]))
 
-# Need to separate spcode into spcode and replicate
+library(ggplot2)
+ggplot(vol.sum, aes(group, sum_low_fp)) + geom_point() + facet_grid(. ~ spcode)
+ggplot(filter(vol, flash_point<100), aes(spcode, flash_point, color=group)) +
+    geom_jitter(aes(size=area))
 
-library(tidyr)
-voldf <- vol %>%
-          separate(spcode, c("spcode", "replicate"), "\\.")
-
-voldf <- voldf[, c(4, 5, 1, 2, 3, 6)]
-
-voldf< - voldf[complete.cases(voldf),]
