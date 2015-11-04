b9f556fb664b6ae9752c48eef6e59febf41eb72a
Dylan W. Schwilk <dylan@schwilk.org>
2015-11-04 11:01:29 -0600

    Moisture plots to use current theme and data
    also wrap up repetitive code in a function.

--- scripts/burn-moist.R
+++ scripts/burn-moist.R
@@ -2,61 +2,35 @@
 
 # read data
 burnt <- read.csv("../data/moisture/burn_moisture_trials.csv")
-
 burnt$spcode <- factor(burnt$spcode)
 
 source("theme-opts.R")
-source("read-decomp.R")
+#source("read-decomp.R")
 
-# Plots of flammability and moisture levels
+## Plots of flammability and moisture levels
 
-ggplot(burnt, aes(moisture, log(t2ignit), colour=spcode)) +
+## DWS: why show lines when not sig? Why expect linear?
+flammoistplot <-  function(resp.var, ylab) {
+    pred.var = "moisture"
+    r <- ggplot(burnt, aes_string(pred.var, resp.var, colour="spcode")) +
         geom_point(size=1.5) +
         scale_colour_discrete(name="Species", breaks=spbreaks, labels=labels1) +
-        xlab("Moisture content (%)") + ylab("Ignitability (s)") +
-        theme_bw() + themeopts +
+        xlab("Moisture content (%)") + ylab(ylab) +
+        pubtheme +
         stat_smooth(data=burnt, method="lm", se=FALSE, size=1)
+    ggsave(file.path("..", "results", "plots",
+                     paste(pred.var, "_", resp.var, ".png", sep="")), plot=r)
+    # Why png? Why not vector format?
+    return(r)
+}
 
-ggsave("../results/plots/moisture_ignit.png", width=9, height=5, dpi=ppi)
+flammoistplot("log(t2ignit)", "Ignitability (s)")
+flammoistplot("spread", "Spread rate (mm/s)")
+flammoistplot("combust", "Combustability (mm)")
+flammoistplot("consume", "Consumability (%)")
+flammoistplot("sustain", "Sustainability (s)")
 
-ggplot(burnt, aes(moisture, spread, colour=spcode)) +
-        geom_point(size=1.5) +
-        scale_colour_discrete(name="Species", breaks=spbreaks, labels=labels1) +
-        xlab("Moisture content (%)") + ylab("Spread rate (mm/s)") +
-        theme_bw() + themeopts +
-        stat_smooth(data=burnt, method="lm", se=FALSE, size=1)
-
-ggsave("../results/plots/moisture_spread.png", width=9, height=5, dpi=ppi)
-
-
-ggplot(burnt, aes(moisture, combust, colour=spcode)) +
-        geom_point(size=1.5) +
-        scale_colour_discrete(name="Species", breaks=spbreaks, labels=labels1) +
-        xlab("Moisture content (%)") + ylab("Combustability (mm)") +
-        theme_bw() + themeopts +
-        stat_smooth(data=burnt, method="lm", se=FALSE, size=1)
-
-ggsave("../results/plots/moisture_combust.png", width=9, height=5, dpi=ppi)
-
-ggplot(burnt, aes(moisture, consum, colour=spcode)) +
-        geom_point(size=1.5) +
-        scale_colour_discrete(name="Species", breaks=spbreaks, labels=labels1) +
-        xlab("Moisture content (%)") + ylab("Consumability (%)") +
-        theme_bw() + themeopts +
-        stat_smooth(data=burnt, method="lm", se=FALSE, size=1)
-
-ggsave("../results/plots/moisture_consum.png", width=9, height=5, dpi=ppi)
-
-ggplot(burnt, aes(moisture, sustain, colour=spcode)) +
-        geom_point(size=1.5) +
-        scale_colour_discrete(name="Species", breaks=spbreaks, labels=labels1) +
-        xlab("Moisture content (%)") + ylab("Sustainability (s)") +
-        theme_bw() + themeopts +
-        stat_smooth(data=burnt, method="lm", se=FALSE, size=1)
-
-ggsave("../results/plots/moisture_sustain.png", width=9, height=5, dpi=ppi)
-
-burntr <- merge(subset(decomp.sum2, year=="0"), burnt, by="spcode", sort=F)
+# burntr <- merge(subset(decomp.sum, year=="0"), burnt, by="spcode", sort=F)
 
 ## Binomial analysis of ignition
 
@@ -65,6 +39,7 @@ ggplot(burnt, aes(moisture, ignit, colour=spcode)) +
         stat_smooth(method="glm", family="binomial", se=F) +
         scale_colour_manual(name="Species", breaks=spbreaks, labels=labels2, 
                             values=cbcolours1) +
-        theme_bw()
+        pubtheme
 
 fit <- glm(ignit~moisture + spcode + rh + temp, data=burnt, family=binomial())
+summary(fit)
