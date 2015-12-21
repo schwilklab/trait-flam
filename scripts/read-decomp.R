9692e885471cbe0ba25e0f0d82c5a7aaf7e5ff72
FieryMedusa <rita.quinones-magalhaes@ttu.edu>
2015-12-20 21:34:04 -0600

    Added code and refactoring
    Added code to read in LAI and CN data, and refactored the code for
    readability

--- scripts/read-decomp.R
+++ scripts/read-decomp.R
@@ -2,7 +2,8 @@
 
 ## 1. Read in files, combine, create factors
 ## 2. Create summary statistics data frame
-## 3. Exports three data frames, "decomp", "decomp.sum" and "decomp.sum2"
+## 3. Exports five data frames, "decomp", "decomp.sum", "decomp.sum2",
+##                                    "flamdecomp", and "flamdecomp.sum"
 
 source("read-flam.R")
 library(dplyr)
@@ -12,9 +13,12 @@ source("dist-funcs.R")
 
 
 ## The files are separate due to their size 
-Y0 <- read.csv("../data/decomp/Decomp_Y0.csv", na.strings = c("","NA"), stringsAsFactors=FALSE)
-Y1 <- read.csv("../data/decomp/Decomp_Y1.csv", na.strings = c("","NA"), stringsAsFactors=FALSE)
-Y2 <- read.csv("../data/decomp/Decomp_Y2.csv", na.strings = c("","NA"), stringsAsFactors=FALSE)
+Y0 <- read.csv("../data/decomp/Decomp_Y0.csv", na.strings = c("","NA"),
+               stringsAsFactors=FALSE)
+Y1 <- read.csv("../data/decomp/Decomp_Y1.csv", na.strings = c("","NA"),
+               stringsAsFactors=FALSE)
+Y2 <- read.csv("../data/decomp/Decomp_Y2.csv", na.strings = c("","NA"),
+               stringsAsFactors=FALSE)
 
 # rbind em up
 decomp <- bind_rows(Y0,Y1,Y2)
@@ -26,20 +30,24 @@ rm(list = c("Y0","Y1", "Y2"))
 decomp <- filter(decomp, ! (tag==126 & year > 0))
 decomp$larea <- decomp$l * decomp$w
 decomp$lvol <- decomp$larea * decomp$t
+decomp$lt <- decomp$l/decomp$t
 
-# Using ddply to summarize by different measures, and then using model
-# selection to determine which measure is the best to use. It is mean(l)
-
-## DWS: I don't see evidence that mean l is best predictor. of what? where is
-## proof?
+# Using ddply to summarize by different measures
 
 q90 <- function(x) { quantile(x, .90, na.rm=TRUE)}
 q10 <- function(x) { quantile(x, .10, na.rm=TRUE)} 
 N <- function(x) {sum(!is.na(x))}
 
-wt <- read.csv("../data/decomp/Decomp_weight.csv", na.strings = c("","NA"), stringsAsFactors=FALSE)
+wt <- read.csv("../data/decomp/Decomp_weight.csv", na.strings = c("","NA"),
+               stringsAsFactors=FALSE)
 wt <- filter(wt, tag!=126)
-wt$drate <- -log(wt$wf/wt$wi)/wt$year
+wt$wdrate <- -log(wt$wf/wt$wi)/wt$year
+CN <- read.csv("../data/decomp/CNratio.csv", na.strings = c("","NA"), 
+               stringsAsFactors=FALSE)
+CN$CNratio <- CN$C/CN$N/CN$mass
+
+LAI <- read.csv("../data/decomp/SEKI_LAI.csv", na.strings = c("","NA"), 
+                stringsAsFactors=FALSE)
 
 # summarize by litter bag
 decomp.sum <- decomp %>% group_by(tag, spcode, year, alt, asp) %>%
@@ -55,9 +63,11 @@ decomp.sum <- decomp %>% group_by(tag, spcode, year, alt, asp) %>%
     left_join(species)
 
 # just year zero with flam trial results by species
-## flamdecomp <- decomp.sum %>% filter(year==0) %>% select(l_mean, t_mean, display.name) %>%
-##     group_by(display.name) %>% summarize(lt_mean = mean(l_mean/t_mean, na.rm=TRUE),
+## flamdecomp <- decomp.sum %>% filter(year==0) %>% select(l_mean, t_mean, display.name) 
+##                    %>% group_by(display.name) 
+##                    %>% summarize(lt_mean = mean(l_mean/t_mean, na.rm=TRUE),
 ##                                          lt_sd=sd(l_mean/t_mean, na.rm=TRUE))
+
 flamdecomp <- merge(flam.sp.avg, decomp.sum, all.x=TRUE)
 
 flamdecomp.sum <- flamdecomp %>% filter(year==0) %>%
@@ -74,4 +84,3 @@ flamdecomp.sum <- flamdecomp %>% filter(year==0) %>%
                                          lt_mean= mean(l_mean/t_mean),
                                          lt_sd  = sd(l_mean/t_mean)
                                          )
-
