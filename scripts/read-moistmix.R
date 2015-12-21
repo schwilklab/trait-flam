6abcba0138122cc2fffe3f34c1ab48c123a5a622
FieryMedusa <rita.quinones-magalhaes@ttu.edu>
2015-12-20 21:35:20 -0600

    Added code
    Added code to read in and use the data from the predicted values of the
    mixtures

--- scripts/read-moistmix.R
+++ scripts/read-moistmix.R
@@ -1,5 +1,9 @@
 # read data
+
 mmc <- read.csv("../data/moisture/burn_moisture_mixtures.csv")
+pred_mmc <- read.csv("../data/moisture/pred_dry_mix.csv")
+
+obs_pred <- merge(pred_mmc, mmc, by=c("hour", "spcode"))
 
 source("theme-opts.R")
 
@@ -16,3 +20,13 @@ ggplot(mmc, aes(drying_time, MC_dry, colour=spcode)) +
   geom_smooth(method="glm", family=gaussian(link="log"))
 
 ggsave("../results/plots/moisturemix.png", width=9, height=5, dpi=ppi)
+
+ggplot(obs_pred, aes(pred_MC_dry, MC_dry, colour=spcode)) +
+  geom_point(aes(shape=spcode), size=3) +
+  scale_shape_discrete(name="Species", breaks=mspbreaks) +
+  scale_colour_discrete(name="Species", breaks=mspbreaks) +
+  xlab("Predicted moisture content (%)") + ylab("Observed moisture content (%)") +
+  theme_bw()+ 
+  geom_abline(intercept=0, slope=1)
+
+ggsave("../results/plots/mcmix_pred_obs.pdf", width=9, height=5, dpi=ppi)
