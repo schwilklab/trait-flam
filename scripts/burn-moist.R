8ede5561d1ee6b337a1f837206a69507a1e14402
FieryMedusa <rita.quinones-magalhaes@ttu.edu>
2015-12-20 21:36:14 -0600

    Correction to code
    Corrected the code so that it uses the moisture value on a dry basis,
    instead of wet

--- scripts/burn-moist.R
+++ scripts/burn-moist.R
@@ -9,18 +9,17 @@ source("read-decomp.R")
 
 ## Plots of flammability and moisture levels
 
-## DWS: why show lines when not sig? Why expect linear?
 flammoistplot <-  function(resp.var, ylab) {
-    pred.var = "moisture"
+    pred.var = "moisture_dry"
     r <- ggplot(burnt, aes_string(pred.var, resp.var, colour="spcode")) +
         geom_point(size=1.5) +
-        scale_colour_manual(name="Species", breaks=spbreaks, labels=labels1, values=cbcolours2) +
+        scale_colour_manual(name="Species", breaks=spbreaks, labels=labels2,
+                            values=cbcolours1) +
         xlab("Moisture content (%)") + ylab(ylab) +
         pubtheme +
         stat_smooth(data=burnt, method="lm", se=FALSE, size=1)
     ggsave(file.path("..", "results", "plots",
-                     paste(pred.var, "_", resp.var, ".png", sep="")), plot=r)
-    # Why png? Why not vector format?
+                     paste(pred.var, "_", resp.var, ".pdf", sep="")), plot=r)
     return(r)
 }
 
@@ -34,12 +33,17 @@ flammoistplot("sustain", "Sustainability (s)")
 
 ## Binomial analysis of ignition
 
-ggplot(burnt, aes(moisture, ignit, colour=spcode)) +
+ggplot(burnt, aes(moisture_dry, ignit, colour=spcode)) +
         geom_point(size=1.5) +
         stat_smooth(method="glm", family="binomial", se=F) +
+      	scale_x_continuous(breaks=xbreaks, limits=c(10,65)) +
+        xlab("Moisture content (%)") + ylab("Probability of ignition") +
         scale_colour_manual(name="Species", breaks=spbreaks, labels=labels2, 
                             values=cbcolours1) +
-        pubtheme
+        pubtheme + theme(axis.title.x = element_text(size=16),
+                         axis.text.x  = element_text(size=14),
+                         axis.title.y = element_text(size=16),
+                         axis.text.y  = element_text(size=14))
 
 fit <- glm(ignit~moisture + spcode + rh + temp, data=burnt, family=binomial())
 summary(fit)
