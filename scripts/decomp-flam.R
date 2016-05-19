eee9a836fb5bedf74a0b6a59c1deec5600617863
FieryMedusa <rita.quinones-magalhaes@ttu.edu>
2016-05-18 21:40:20 -0500

    Fixed redundant code
    Fixed some syntax and eliminated redundant code

--- scripts/decomp-flam.R
+++ scripts/decomp-flam.R
@@ -13,7 +13,6 @@ library(dplyr)
 library(lme4)
 library(AICcmodavg)
 
-flamdecomp.sum.y0 <- filter(flamdecomp.sum, year == 0)
 # DWS: you should fit on original flam data in a nested model. By fitting on
 # means you are overestimating certainty. But may be fine for now
 
@@ -35,24 +34,7 @@ ggplot(flamdecomp.sum.y0, aes(l_mean/t_mean, spread.mean)) + geom_point()
 ggplot(flamdecomp.sum.y0, aes(bulk.mean, spread.mean)) + geom_point()
 # yes.
 
-# Creating new dataframe with the predicted spread rate (y0, y1, y2))
-pred.allyears <- decomp.sum  %>% ungroup() %>%
-    select(spcode, year, l_mean, w_mean, t_mean, larea_mean)
-
-pred.allyears$pred.spread <- predict(lmfit2, newdata=pred.allyears)
-
-pred.allyears <- pred.allyears %>%
-    select(spcode, year, pred.spread, l_mean, t_mean) %>%
-    group_by(spcode, year) %>%
-    summarise(lt_mean = mean(l_mean/t_mean),
-              pred.spread_mean = mean(pred.spread))
-
-#flamdecomp.sum$pred.spread <- predict(lmfit2) # pred fitted values
-
-pred.allyears <- left_join(pred.allyears, select(flamdecomp.sum, spcode, year, spread.mean))
-pred.allyears$spread.mean[pred.allyears$year > 0] <- pred.allyears$pred.spread_mean[pred.allyears$year > 0]
-
-# Creating new dataframe with the predicted spread rate after decomposition (just year 1)
+# Creating new dataframe with the predicted spread rate after decomposition (year 1)
 
 pred.y1 <- decomp.sum %>%  filter(year==1) %>% ungroup() %>%
     select(spcode, l_mean, w_mean, t_mean, larea_mean)
@@ -78,3 +60,11 @@ pred.y1sum <- pred.y1 %>% select(spcode, pred_spread, l_mean, w_mean, larea_mean
     )
 
 pred.y1sum <- pred.y1sum %>% left_join(species)
+
+#flamdecomp.sum$pred.spread <- predict(lmfit2) # pred fitted values
+
+## flamdecomp.sum <- left_join(flamdecomp.sum, select(pred.y1, spcode, pred.spread_mean, pred.spread_sd), by="spcode")
+
+
+
+
