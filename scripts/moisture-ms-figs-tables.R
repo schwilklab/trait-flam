bf8a39b60085abddf09f6029dbed355a782822cc
Dylan W. Schwilk <dylan@schwilk.org>
2019-04-22 13:39:44 -0500

    Start on LaTeX manuscript
    Add latex file based on MDPI template and MDPI latex class definitions.
    See https://www.mdpi.com/authors/latex

--- scripts/moisture-ms-figs-tables.R
+++ scripts/moisture-ms-figs-tables.R
@@ -1,43 +1,44 @@
 # moistsingles-plots
 
+RESULTS <- "../ms/figs_tables/"
+
 source("theme-opts.R")
 source("dry-down.R")
 source("burn-moist.R")
 
 ## Figure 1: Plotting dry-down
-
 xbreaks <- seq(0, 144, 24)
 ybreaks <- seq(0, 700, 50)
 
 ggplot(mc, aes(hour, MC_dry, group=display.name, color=genus)) +
   geom_point(size=1.2, alpha=0.7, stroke=0) +
+  geom_smooth(method="glm",
+              method.args=list(family=gaussian(link="log")), se=FALSE, size =0.6) +
   scale_colour_manual(values=schwilkcolors) +
   xlab("Hours since wetting") + ylab("Moisture by dry weight (%)") +
   scale_x_continuous(breaks=xbreaks) +
   scale_y_continuous(breaks=ybreaks) +
   pubtheme.nogridlines +
-  geom_smooth(method="glm",
-              method.args=list(family=gaussian(link="log")), se=FALSE, size =0.6) +
   theme(legend.position=c(0.75, 0.86),
         legend.title=element_blank(),
         legend.text = element_text(family=fontfamily, size=smsize, face="italic"))
  #       legend.key.height=unit(smsize,"pt"))
 
-ggsave("../results/moisture_ms/fig1_drydown-curves.pdf", width=col1, height=col1, units="cm")
+ggsave(file.path(RESULTS, "fig1_drydown-curves.pdf"), width=col1, height=col1, units="cm")
 
 ggplot(mc, aes(hour, MC_dry, group=display.name, color=genus)) +
   geom_point(size=1.2, alpha=0.7, stroke=0) +
+  geom_smooth(method="lm", se=FALSE, size =0.6) +
   scale_colour_manual(values=schwilkcolors) +
   xlab("Hours since wetting") + ylab("Moisture by dry weight (%)") +
   scale_x_continuous(breaks=xbreaks) +
   scale_y_log10() +
   pubtheme.nogridlines +
-  geom_smooth(method="lm", se=FALSE, size =0.6) +
   theme(legend.position=c(0.75, 0.86),
         legend.title=element_blank(),
         legend.text = element_text(family=fontfamily, size=smsize, face="italic"))
 
-ggsave("../results/moisture_ms/fig1_drydown-curves_logged.pdf", width=col1, height=col1, units="cm")
+ggsave(file.path(RESULTS, "fig1_drydown-curves_logged.pdf"), width=col1, height=col1, units="cm")
 
 ###############################################################################
 ## Figures 3-4: Plots of flammability and moisture levels
@@ -45,34 +46,33 @@ ggsave("../results/moisture_ms/fig1_drydown-curves_logged.pdf", width=col1, heig
 ## Flammability and time since wetting by genus
 #################################################
 
-burnt1 <- subset(burnt,ignit==1)
-
-ggplot(burnt, aes(hour, t2ignit, color=genus)) +
+ggplot(na.omit(burnt), aes(hour, t2ignit, color=genus)) +
   geom_jitter(width=2, size=1.5, alpha=0.7, stroke=0) +
+  geom_smooth(method="lm", se=FALSE, size=1.2) +
   scale_colour_manual(values=schwilkcolors) +
   xlab("Time since wetting (hr)") + ylab("Time to ignition (s)") +
   scale_x_continuous(breaks=xbreaks) +
   scale_y_log10() +
   pubtheme.nogridlines +
-  stat_smooth(data=burnt, method="lm", se=FALSE, size=1.2) +
   theme(legend.position=c(0.75, 0.86),
     legend.title=element_blank(),
     legend.text = element_text(family=fontfamily, size=smsize, face="italic"))
 
-ggsave("../results/moisture_ms/fig2_ignit_by_time.pdf", width=col1, height=col1, units="cm")
+ggsave(file.path(RESULTS, "fig2_ignit_by_time.pdf"), width=col1, height=col1, units="cm")
 
-ggplot(burnt, aes(hour, spread, group=spcode, color=genus)) +
+ggplot(subset(burnt, ignit==1),
+       aes(hour, spread, color=genus)) +
   geom_jitter(width=2, size=1.5, alpha=0.7, stroke=0) +
-  scale_colour_manual(values=schwilkcolors) +
+  geom_smooth(method="lm", se=FALSE, size=1.2) +
+  scale_colour_manual(values=schwilkcolors, drop=TRUE) +
   xlab("Time since wetting (hr)") +  ylab("Spread rate (mm/s)") +
   scale_x_continuous(breaks=xbreaks) +
   #scale_y_log10() +
   pubtheme.nogridlines +
-  stat_smooth(data=burnt, method="lm", se=FALSE, size=1.2) +
   theme(legend.position=c(.3, .86),
         legend.title=element_blank())
 
-ggsave("../results/moisture_ms/fig3_spread_by_time.pdf", width=col1, height=col1, units="cm")
+ggsave(file.path(RESULTS, "fig3_spread_by_time.pdf"), width=col1, height=col1, units="cm")
 
 
 ###############################################################################
@@ -90,28 +90,18 @@ ggplot(burnt, aes(actualMC_dry, t2ignit, color=genus)) +
         legend.spacing.y=unit(0,"cm"),
         legend.text = element_text(family=fontfamily, size=smsize-1, face="italic"),
         legend.title=element_blank())
-ggsave("../results/moisture_ms/actualMC_ignit.pdf", width=col1, height=col1, unit="cm")
+ggsave(file.path(RESULTS, "actualMC_ignit.pdf"), width=col1, height=col1, unit="cm")
 
 
-ggplot(burnt, aes(actualMC_dry, spread, color=genus)) +
+ggplot(burnt1, aes(actualMC_dry, spread, color=genus)) +
   geom_point(size=1.5, alpha=0.7, stroke=0) +
   scale_colour_manual(values=schwilkcolors) +
   xlab("Moisture content (%)") + ylab("Spread rate (mm/s)") +
   pubtheme.nogridlines +
-  stat_smooth(data=burnt, method="lm", se=FALSE, size=0.8) +
+  stat_smooth(data=burnt, method="lm", se=FALSE, size=0.8, drop=TRUE) +
   theme(legend.position=c(.75, .86),
         legend.spacing.y=unit(0,"cm"),
         legend.text = element_text(family=fontfamily, size=smsize-1, face="italic"),
         legend.title=element_blank())
-ggsave("../results/moisture_ms/actualMC_spread.pdf", width=col1, height=col1, unit="cm")
+ggsave(file.path(RESULTS, "actualMC_spread.pdf"), width=col1, height=col1, unit="cm")
 
-
-ggplot(burnt1, aes(actualMC_dry, spread, colour=genus)) +
-  geom_point(size=2) +
-  scale_colour_manual(name="Genus", breaks=gnsbreaks, labels=labels3, values=mycolours3) +
-  xlab("Moisture content (%)") + ylab("Spread rate (mm/s)") +
-  ritatheme +
-  stat_smooth(data=burnt1, method="lm", se=FALSE, size=1.5)
-
-ggsave("../results/plots/actualMC_Spread.pdf", width=9, height=6, dpi=ppi)
-ggsave("../results/plots/actualMC_Spread.png", width=9, height=6, dpi=ppi)
